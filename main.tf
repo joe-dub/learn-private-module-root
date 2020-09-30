a8f4f49114b98cbc4d3bc76c3b29e3d2ca391c8b
joe-dub <joedubail@invsionapp.com>
2020-09-30 14:39:24 -0700

    updated

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = var.region
+  profile = "playground"
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/abide/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
